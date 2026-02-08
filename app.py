e00958606355632b5b93326a3c185ced48907ff0
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-02-07 19:38:46 -0700

    Add a map to dashboard

--- app.py
+++ app.py
@@ -72,3 +72,76 @@ with ui.layout_columns(col_widths=[6, 6]):
             )
             fig.update_traces(textinfo="percent+label")
             return fig
+
+
+## Map plot ##
+def prepare_flight_paths(df_in):
+
+    df_in = df_in.with_columns(
+        (pl.col("C_PAX_CNT") + pl.col("Y_PAX_CNT")).alias("Affected_Passengers")
+    )
+
+    valid_flights = df_in.drop_nulls(
+        subset=["ORIG_LAT", "ORIG_LONG", "DEST_LAT", "DEST_LONG"]
+    )
+
+    origins = valid_flights.select(
+        pl.col("DEP_KEY"),
+        pl.col("ORIG_LAT").alias("lat"),
+        pl.col("ORIG_LONG").alias("lon"),
+        pl.lit("Origin").alias("Type"),
+        pl.col("Affected_Passengers"),
+    )
+    dests = valid_flights.select(
+        pl.col("DEP_KEY"),
+        pl.col("DEST_LAT").alias("lat"),
+        pl.col("DEST_LONG").alias("lon"),
+        pl.lit("Dest").alias("Type"),
+        pl.col("Affected_Passengers"),
+    )
+    return pl.concat([origins, dests], how="vertical")
+
+
+df_plot = prepare_flight_paths(df_affected_flights)
+
+with ui.card(full_screen=True, height="500px"):
+    ui.card_header("Affected Flight Routes")
+
+    @render_plotly
+    def flight_map():
+        fig = px.line_mapbox(
+            df_plot,
+            lat="lat",
+            lon="lon",
+            line_group="DEP_KEY",
+            color="Type",
+            mapbox_style="carto-darkmatter",
+            zoom=1,
+            center={"lat": 20, "lon": 0},
+        )
+
+        fig_markers = px.scatter_mapbox(
+            df_plot,
+            lat="lat",
+            lon="lon",
+            size="Affected_Passengers",
+            size_max=15,
+            color="Type",
+            hover_name="DEP_KEY",
+            mapbox_style="carto-darkmatter",
+        )
+
+        fig.add_traces(fig_markers.data)
+
+        fig.update_layout(
+            margin={"r": 0, "t": 0, "l": 0, "b": 0},
+            showlegend=False,
+            paper_bgcolor="rgba(0,0,0,0)",
+        )
+
+        fig.update_traces(
+            selector=dict(type="scattermapbox", mode="lines"),
+            line=dict(width=2, color="#d3462d"),
+        )
+
+        return fig
